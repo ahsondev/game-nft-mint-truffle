e855d57c4785c2cffa8ce81a80348b58542b17dc
unknown <golden.star0228@gmail.com>
2021-10-13 17:49:20 -0400

    set port in node.js

--- server/config.js
+++ server/config.js
@@ -2,5 +2,4 @@ const config = require('dotenv-flow').config().parsed
 
 module.exports = {
   ...config,
-  PORT: 4000
 }
--- server/index.js
+++ server/index.js
@@ -2,8 +2,7 @@ const config = require('./config')
 const express = require('./services/express')
 const routes = require('./routes')
 
-const port = config.PORT || 4000
-// const port = parseInt(config.PORT, 10) || 3000
+const port = process.env.PORT || 4000
 const app = express(routes)
 
 app.listen(port, (err) => {
